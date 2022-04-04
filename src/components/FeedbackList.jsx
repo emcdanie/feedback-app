583092c98fc568b22dc516fda32180af91b81c78
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-04 17:58:11 +0200

    Fade animation with framer motion
    add fade animation with framer morion to the FeedbackList items

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,4 +1,7 @@
+import { motion, AnimatePresence } from 'framer-motion'
+import PropTypes from 'prop-types'
 import FeedbackItem from "./FeedbackItem"
+
 
 function FeedbackList({feedback, handleDelete}) {
    if (!feedback && (!feedback || feedback.length === 0)) {
@@ -7,14 +10,31 @@ function FeedbackList({feedback, handleDelete}) {
 
   return (
     <div className='feedback.list'>
+         <AnimatePresence>
         {feedback.map((item) => (
-            <FeedbackItem 
-            key={item.id} 
-            item={item} 
-            handleDelete={handleDelete}/>
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            layout
+        >
+            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete}/>
+          </motion.div>
         ))}
+      </AnimatePresence>
     </div>
   )
+        }
+FeedbackList.propTypes ={
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id:PropTypes.number.isRequired,
+      text:PropTypes.string.isRequired,
+      rating: PropTypes.number.isRequired
+    })
+  ),
 }
+
 
 export default FeedbackList
