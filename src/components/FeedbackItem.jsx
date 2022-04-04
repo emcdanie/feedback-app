b648f826071e0006de0fbe0e48a82e1f57016521
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-04 14:59:24 +0200

    FeedbackStats Component & Reactivity
    created feedback stats component

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -6,7 +6,7 @@ function FeedbackItem({item, handleDelete}) {
   return (
     <Card>
         <div className='num-display'>{item.rating}</div>
-        <button onClick={() => handleDelete(item.ed)} className="close">
+        <button onClick={() => handleDelete(item.ed)} className='close'>
             <FaTimes color="purple" />
         </button>
         <div className='text-diplay'>{item.text}</div>
