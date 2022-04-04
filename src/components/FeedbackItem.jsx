15686804e2357198280977fced890d29b14fc074
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-04 14:36:40 +0200

    Events & Prop Drilling
    Create prop of onDelete for cards .

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,10 +1,14 @@
+import { FaTimes, FaEdit } from 'react-icons/fa'
 import PropTypes from "prop-types"
 import Card from "./shared/Card"
 
-function FeedbackItem({item}) {
+function FeedbackItem({item, handleDelete}) {
   return (
     <Card>
         <div className='num-display'>{item.rating}</div>
+        <button onClick={() => handleDelete(item.ed)} className="close">
+            <FaTimes color="purple" />
+        </button>
         <div className='text-diplay'>{item.text}</div>
     </Card>
   )
