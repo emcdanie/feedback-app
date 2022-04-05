669511b3afa61636344d3d1f98fe2be419e4d41d
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-05 13:04:56 +0200

    Edit Feedback Event
    edit Feedback even added to feedback context and form

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -20,25 +20,38 @@ export const FeedbackProvider =({children}) => {
      rating: 7,
    },
 ])
+  const [ feedbackEdit, setFeedbackEdit] = useState({
+    item:{},
+    edit: false
+  })
+//add feedback
   const addFeedback = (newFeedback) => {
     newFeedback.id = uuidv4()
     setFeedback([newFeedback, ...feedback])
 
   }
 
-
+//delete Feedback
  const deleteFeedback = (id) => {
    if (window.confirm("Are you sure you want to delete?")) {
      setFeedback(feedback.filter((item) => item.id !== id))
    }
  }
 
+ //Set Item to be updated
+const editFeedback = (item) => {
+  setFeedbackEdit({
+    item,
+    edit: true
+  })
+}
 return (
     <FeedbackContext.Provider
       value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
        
       }}
     >
--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,10 +1,19 @@
-import Card from "../components/shared/Card"
-function AboutPage() {
+import { Link } from 'react-router-dom'
+import Card from '../components/shared/Card'
+
+function AboutPage(props) {
   return (
-    <div className="about">
-        <h1>About this Project</h1>
+    <Card>
+      <div className='about'>
+        <h1>About This Project</h1>
         <p>Completed this project to learn best practices of React States, Hooks, Prop, Global Css Styling, Api and HTTPs.  </p>
-    </div>
+        <p>Version: 1.0.0</p>
+
+        <p>
+          <Link to='/'>Back To Home</Link>
+        </p>
+      </div>
+    </Card>
   )
 }
 
