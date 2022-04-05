6f7d76d110162cf1136a8c769392b7314ed9a1cc
emcdanie <66241957+emcdanie@users.noreply.github.com>
2022-04-05 14:14:43 +0200

    fetch data json -serverbackend

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,8 +1,4 @@
-import {
-  createContext,
-  useState,
-  useEffect
-} from 'react'
+import {createContext,useState,useEffect} from 'react'
 
 const FeedbackContext = createContext()
 
@@ -22,7 +18,7 @@ export const FeedbackProvider = ({
 
   // Fetch feedback
   const fetchFeedback = async () => {
-    const response = await fetch(`/feedback?_sort=id&_order=desc`)
+    const response = await fetch(`http:localhost:5000/feedback?_sort=id&_order=desc`)
     const data = await response.json()
 
     setFeedback(data)
@@ -100,8 +96,7 @@ export const FeedbackProvider = ({
     } >
     {
       children
-    } <
-    /FeedbackContext.Provider>
+    } </FeedbackContext.Provider>
   )
 }
 
